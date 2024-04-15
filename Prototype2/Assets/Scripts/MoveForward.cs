df0e64751296955baadae14ab6500e2351065231
joshuA-354 <142622574+joshuA-354@users.noreply.github.com>
2024-04-15 11:57:18 -0700

    detect collision and move foward

--- Prototype2/Assets/Scripts/MoveForward.cs
+++ Prototype2/Assets/Scripts/MoveForward.cs
@@ -1,19 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
-    public float speed = 40.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField] public float speed = 40.0f;
+    
+    
         
-    }
+    
 
     // Update is called once per frame
-    void Update()
+    void Update() 
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.Translate(Vector3.forward * (speed * Time.deltaTime));
     }
 }
