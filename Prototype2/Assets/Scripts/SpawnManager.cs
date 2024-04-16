230b3981cf736c80ef63b9acd9be5d5771e55632
joshuA-354 <142622574+joshuA-354@users.noreply.github.com>
2024-04-16 12:04:10 -0700

    player controller spawn manager

--- Prototype2/Assets/Scripts/SpawnManager.cs
+++ Prototype2/Assets/Scripts/SpawnManager.cs
@@ -1,35 +1,35 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
     //animal prefabs
-    public GameObject[] animalPrefabs;
+    [SerializedField] private GameObject[] animalPrefabs;
     //x range
-    private float spawnRangeX = 20.0f;
+    [SerializedFieldprivate] float spawnRangeX = 20.0f;
     //z position
-    private float spawnPosZ = 20.0f;
+    [SerializedField]private float spawnPosZ = 20.0f;
 
     //Start Delay
-    private float startDelay = 2.0f;
+    [SerializedField]private float startDelay = 2.0f;
     //Spawn Interval
-    private float spawnInterval = 1.5f;
+    [SerializedField]private float spawnInterval = 1.5f;
+
 
     //Start is called before the first frame
     void Start()
     {
-        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+        InvokeRepeating(private const string PLAYER_TAG = "Player";, startDelay, spawnInterval);
     }
 
     //Method to spawn animals
-    private void SpawnRandomAnimal()
+    private const string P ()
     {
         // Choose a random number
         int animalIndex = Random.Range(0, animalPrefabs.Length);
         //Spawn Position
-        Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0.0f, spawnPosZ);
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),( 0.0f, spawnPosZ);
         //Spawn Animal
-        Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
+        Instantiate(animalPrefabs[animalIndex], (spawnPosition, animalPrefabs[animalIndex].transform.rotation);
     }
 }
